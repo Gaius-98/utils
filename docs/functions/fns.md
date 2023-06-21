349f6218e5e921b8370d4f0da0b99769bbe637dd
Grace <2388838628@qq.com>
2023-06-21 15:24:08 +0800

    feat：增加扁平化、反扁平化函数

--- docs/functions/fns.md
+++ docs/functions/fns.md
@@ -29,3 +29,11 @@ fnxxx()
     word|string|--|需要修改的字符串
     startIndex|number|0|开始序号
     endIndex|number|1|结束序号
+5. flat 对象扁平化函数
+   参数|类型|默认值|说明
+    --|--|--|----
+    obj|Object|--|需要扁平化的对象
+6. unflat 对象反扁平化函数
+   参数|类型|默认值|说明
+    --|--|--|----
+    obj|Object|--|需要反扁平化的对象    
--- packages/functions/functions.ts
+++ packages/functions/functions.ts
@@ -1,3 +1,4 @@
+import { obj } from '../../types/common'
 /**
  * 滚动container 定位容器
  * @param container -- Element dom的容器
@@ -63,4 +64,111 @@ const getUpperCase = (word:string, startIndex = 0, endIndex = 1) => {
   }
   return word.slice(startIndex, endIndex).toLocaleUpperCase() + word.slice(endIndex)
 }
-export { scrollToDom, getVarType, getLowerCase, getUpperCase }
+
+/**
+ * 对象扁平化
+ * @params {obj} 要扁平化的对象
+ * @return {obj} 扁平化后的对象+
+ * @example flat({ a:{ b:'1' } }) => {a.b:'1'}
+ */
+const flat = (obj:obj) => {
+  const flatten = (res:obj, curObj:any, curKey:string) => {
+    // 判断当前项是否为数组
+    if (Array.isArray(curObj)) {
+      curObj.forEach((item, idx) => {
+        // 如果是复杂对象则继续
+        if (typeof item == 'object' && item !== null) {
+          flatten(res, item, `${curKey}[${idx}]`)
+        } else {
+          res[`${curKey}[${idx}]`] = item
+        }
+      })
+    } else {
+      Object.keys(curObj).forEach(key => {
+        // 复杂对象嵌套则继续
+        if (typeof curObj[key] === 'object' && curObj[key] !== null) {
+          flatten(res, curObj[key], `${curKey}.${key}`)
+        } else {
+          res[`${curKey}.${key}`] = curObj[key]
+        }
+      })
+    }
+  }
+  const res:obj = {}
+  Object.keys(obj).forEach(key => {
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      flatten(res, obj[key], key)
+    } else {
+      res[key] = obj[key]
+    }
+  })
+  return res
+}
+/**
+ * 对象反扁平化
+ * @params {obj} 扁平化的对象
+ * @return {obj} 反扁平化的对象
+ * @example unflat({a.b:'1'}) => { a:{ b:'1' } }
+ */
+const unflat = (obj:obj) => {
+  const unflatten = (key:string, value:any, res:obj) => {
+    const arr = key.split('.')
+    let tmp = res
+    for (let i = 0; i < arr.length; i++) {
+      let key = arr[i]
+      let nextKey = arr[i + 1]
+      // 这里需要判断key 是否包含[]字符串，如果是则是数组结构
+      if (/\[.+?\]/g.test(key)) {
+        // 可能是多维数组，匹配数组维度
+        let indexs = key.match(/\[(.+?)\]/g) as RegExpMatchArray
+        // 获取数组的key值
+        let _key = key.match(/^(.+?)\[/)![1]
+        // 构造数组需要判断是否已经存在
+        tmp[_key] = tmp[_key] ? tmp[_key] : []
+        let n = tmp[_key]
+              
+        // 构造完数组对数组里面进行构造
+        for (let j = 0; j < indexs.length; j++) {
+          let index = indexs[j].replace(/\[|\]/g, '')
+          let nextIndex = indexs[j + 1]
+              
+          // 数组包含数组
+          if (nextIndex) {
+            // 构造数组需要判断是否已经存在
+                
+            n[index] = n[index] ? n[index] : []
+            // 如果还包含数组，将n指针指向下一个数组
+            n = n[index]
+            // 如果后面还有则构造对象
+          } else if (nextKey) {
+            // 构造对象需要判断是否已经存在
+            n[index] = n[index] ? n[index] : {}
+            tmp = n[index]
+          } else {
+            n[index] = value
+          }
+        }
+        // 不是数组和之前方法保持一致
+      } else if (nextKey) {
+        // 构造对象需要判断是否已经存在
+        tmp[key] = tmp[key] ? tmp[key] : {}
+        tmp = tmp[key]
+      } else {
+        tmp[key] = value
+      }
+    }
+  }
+  const res:obj = {}
+  Object.keys(obj).forEach(key => {
+    unflatten(key, obj[key], res)
+  })
+  return res
+}
+export { 
+  scrollToDom,
+  getVarType,
+  getLowerCase,
+  getUpperCase,
+  flat,
+  unflat,
+}
