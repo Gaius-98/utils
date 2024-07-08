48fdc863d26ce483c7745fe3b0c55af21c312826
Gaius-98 <2388838628@qq.com>
2024-07-08 17:33:17 +0800

    fix(utils): 修改防抖函数

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -2,13 +2,9 @@ export const debounce = (func: Function, wait: number):(...args:any[])=>void => 
   let time: number | null = null
   return (...args:any[]) => {
     if (time) clearTimeout(time)
-    const callNow = !time
     time = window.setTimeout(() => {
       func.apply(this, args)
     }, wait)
-    if (callNow) {
-      func.apply(this, args)
-    }
   }
 }
 export const throttle = (func:Function, wait:number):(...args:any[])=>void => {
